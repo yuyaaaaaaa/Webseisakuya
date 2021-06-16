208a0c1589a8480eb8c85ed0925f59067f6a204d
yuya <78990424+yuyaaaaaaa@users.noreply.github.com>
2021-06-16 12:37:29 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
-$(function() {
+jQuery(function() {
 
   AOS.init({
-    duration: 1000,
+    duration: 700,
   });
 
   $('.humberger-btn').on('click', function() {
     $(this).toggleClass('active');
     $('.nav-list').slideToggle();
+  });
+
+  $('.href-btn').click(function(){
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - 100;
+    $("html,body").animate({scrollTop: position});
+  });
+
+  $('.sitecreate-btn').on('click', function() {
+    $('.sitecreate-modal').slideDown();
+  });
+  $('.sitecreate-close-btn').on('click', function() {
+    $('.sitecreate-modal').slideUp();
+  });
+
+  $('.marketing-btn').on('click', function() {
+    $('.marketing-modal').slideDown();
+  });
+  $('.marketing-close-btn').on('click', function() {
+    $('.marketing-modal').slideUp();
+  });
+
+  $('.support-btn').on('click', function() {
+    $('.support-modal').slideDown();
+  });
+  $('.support-close-btn').on('click', function() {
+    $('.support-modal').slideUp();
   });
 });
